fb31380e34673d3fc6d13607a5d164b644399cc5
Hugo Carrizo Manzoni <hugocmdesign@gmail.com>
2020-01-16 16:41:48 +0100

    add the depency gatsby source datocms, make the query to get the information and show it on the differents pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,11 @@ module.exports = {
         // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: `cf691bf1c7b6c193776fe0eae8e514`
+      },
+    },
   ],
 }
